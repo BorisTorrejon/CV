b53dd12bd106c0290e6668273e016699eaa15b03
Torrejon Boris <65197826+BorisTorrejon@users.noreply.github.com>
2021-06-27 18:50:27 -0300

    laguages
    agregar idiomas a la pagina

---
+++ js/data.js
@@ -0,0 +1,265 @@
+const data= {
+    "languages": [
+        {
+            "language":"español",
+            "lang":"es",
+            "message":"Soy una obra en construcción",
+            "menu":
+            {
+                "title":"MENU",
+                "opt1":"INICIO",
+                "opt2":"EDUCACION",
+                "opt3":"HABILIDAD",
+                "opt4":"EXPERIENCIA LABORAL"
+            },
+            "header":{
+                "bubble":["echo 'Hola Mundo!'","print 'Hola Mundo!'","'Hola Mundo!'"]
+            },
+            "main":{
+                "sect1":{
+                    "title":"Soy Boris",
+                    "subtitle":"Desarrollador Y Analista de Datos",
+                    "message":"Tengo 31 años, nací en Lomas de Zamora, Bs. As., Argentina. Actualmente vivo en Mar del Plata. Me apasiona el desarrollo y análisis de sistemas, tengo facilidad para la resolución de problemas, soy muy exigente conmigo mismo, busco contantemente superarme, tengo facilidad para aprender nuevas herramientas. Trabajo bien en equipo, aunque me desenvuelvo muy bien solo, soy muy proactivo Soy muy nerd! Secreto….actúe en una película, contáctame y tal vez te pase el link de la película. "
+                },
+                "sect2":{
+                    "title":"Educación",
+                    "load":[
+                        {
+                            "title":"Analisis de Datos",
+                            "subtitle":"CODER HOUSE"
+                        },
+                        {
+                            "title":"Ingeniería en Sistemas Informáticos",
+                            "subtitle":"Universidad Atlantida Argentina"
+                        },
+                        {
+                            "title":"Electro-Instalador",
+                            "subtitle":"Escuela de Formación Profesional"
+                        },
+                        {
+                            "title":"ESMET N°1",
+                            "subtitle":"Escuela Secundaria de Educación Técnica"
+                        },
+                        {
+                            "title":"Mecánica del Automotor",
+                            "subtitle":"Escuela de Formación Profesional Municipal"
+                        }
+                    ]
+                },
+                "sect3":{
+                    "title":"Habilidades"
+                },
+                "sect4":{
+                    "title":"Otras Habilidades",
+                    "opt":[
+                        "Resolución de Problemas",
+                        "Análisis Matemático",
+                        "Análisis de Datos",
+                        "Excel Avanzado",
+                        "Power Point Avanzado"
+                    ]
+                },
+                "sect5":{
+                    "title":"Experiencia Laboral",
+                    "subtitle":"Servicio Penitenciario Bs As, Buenos Aires, Argentina",
+                    "jobs":[
+                        {
+                            "title":"Soporte Técnico Informático",
+                            "rank":"DICIEMBRE DE 2018 - AHORA",
+                            "detail":"Desarrollo de Bases de Datos en Vb.net y SQL. Reparación de PC’s, montaje y mantenimiento de RED. Asistencia Usuario de plataformas. "
+                        },
+                        {
+                            "title":"Data Entry",
+                            "rank":"SEPTIEMBRE DE 2015 – DICIEMBRE 2018",
+                            "detail":"Cargar, Modificar y Eliminar datos. Manipulación de Información, búsqueda de incongruencias. "
+                        },
+                        {
+                            "title":"Técnico Electricista",
+                            "rank":"MARZO DE 2011 – SEPTIEMBRE 2015",
+                            "detail":"Mantenimiento de la instalación y prevención de futuras fallas. Instalación de redes eléctricas mono y trifásicas. "
+                        },
+                        {
+                            "title":"Seguridad",
+                            "rank":"ABRIL DE 2009 – MAYO 2011",
+                            "detail":"Seguridad. "
+                        }
+                    ]
+                }
+            }
+        },
+        {
+            "language":"english",
+            "lang":"en",
+            "message":"I'm a work under construction",
+            "menu":
+            {
+                "title":"MENU",
+                "opt1":"HOME",
+                "opt2":"EDUCATION",
+                "opt3":"SKILL",
+                "opt4":"WORK EXPERIENCE"
+            },
+            "header":{
+                "bubble":["echo 'Hello World!'","print 'Hello World!'","'Hello World!'"]
+            },
+            "main":{
+                "sect1":{
+                    "title":"I'm Boris",
+                    "subtitle":"Developer & Data Analyst",
+                    "message":"I'm 31 years old, I was born in Lomas de Zamora, Bs. As., Argentina, but I live in Mar del Plata city. I 'm passionate about systems development and system analysis, I have the ability to solve problems, I'm so demanding with myself that I constantly seek to improve myself, I have the skill to learn new tools. I work well in a teamwork, but i have no problem working alone, I'm proactive. I'm very nerdy! i have a Secret .... I participated in a movie, contact me... and I'll probably send you the link to the movie."
+                },
+                "sect2":{
+                    "title":"Education",
+                    "load":[
+                        {
+                            "title":"Data Analytics",
+                            "subtitle":"CODER HOUSE"
+                        },
+                        {
+                            "title":"Computer Systems Engineering",
+                            "subtitle":"Atlantida Argentina University"
+                        },
+                        {
+                            "title":"Electric-Technicien",
+                            "subtitle":"Professional Training"
+                        },
+                        {
+                            "title":"ESMET N°1",
+                            "subtitle":"School for Technical"
+                        },
+                        {
+                            "title":"Mechanic",
+                            "subtitle":"Professional Training"
+                        }
+                    ]
+                },
+                "sect3":{
+                    "title":"Skill"
+                },
+                "sect4":{
+                    "title":"Other Skill",
+                    "opt":[
+                        "Solve Problems",
+                        "Math Analytics",
+                        "Data Analytics",
+                        "Advance Excel",
+                        "Advance Power Point"
+                    ]
+                },
+                "sect5":{
+                    "title":"Work Experience",
+                    "subtitle":"Penitenciary Service Bs As, Argentina",
+                    "jobs":[
+                        {
+                            "title":"Support Technicien",
+                            "rank":"DECEMBER OF 2018 - NOW",
+                            "detail":"Database development in vb.net & SQL. Repair PCs, Software and Hardware maintenance. Network installation, modification and maintenance. Assistance in retrieving information and resetting passwords. "
+                        },
+                        {
+                            "title":"Data Entry",
+                            "rank":"SEPTEMBER OF 2015 – DECEMBER OF 2018",
+                            "detail":"Upload, modify and delete data. Data manipulation and error finding. "
+                        },
+                        {
+                            "title":"Electric Technicien",
+                            "rank":"MARCH OF 2011 – SEPTEMBER 2015",
+                            "detail":"Maintenance of electrical installation and prevention of future failures. Assembly of new facilities for both general and home networks. Generator set maintenance and start-up. Installation of main and secondary boards. "
+                        },
+                        {
+                            "title":"Security",
+                            "rank":"APRIL OF 2009 – MARCH 2011",
+                            "detail":"Perimeter surveillance. "
+                        }
+                    ]
+                }
+            }
+        },
+        {
+            "language":"français",
+            "lang":"fr",
+            "message":"Je suis un ouvrage en construction",
+            "menu":
+            {
+                "title":"MENU",
+                "opt1":"ACCUEIL",
+                "opt2":"ENSEIGNEMENT",
+                "opt3":"COMPTE",
+                "opt4":"EXPÉRIENCE DE TRAVAIL"
+            },
+            "header":{
+                "bubble":["echo 'Salut Monde!'","print 'Salut Monde!'","'Salut Monde!'"]
+            },
+            "main":{
+                "sect1":{
+                    "title":"Je suis Boris",
+                    "subtitle":"Développeur et analyste de données",
+                    "message":"J'ai 31 ans, je suis né à Lomas de Zamora, Bs. As., Argentine, mais je vis dans la ville de Mar del Plata. Je suis passionné par le développement et l'analyse de systèmes, j'ai le capacité à résoudre des problèmes, je suis tellement exigeant avec moi-même que je cherche constamment à m'améliorer, j'ai la capacité d'apprendre de nouveaux outils. Je travaille bien en équipe, mais je n'ai aucun problème à travailler seul, je suis proactif. Je suis très ringard ! j'ai un secret... J'ai participé à un film, contactez-moi... et je vous enverrai probablement le lien vers le film."
+                },
+                "sect2":{
+                    "title":"Éducation",
+                    "load":[
+                        {
+                            "title":"Data Analytics",
+                            "subtitle":"CODER HOUSE"
+                        },
+                        {
+                            "title":"Ingénierie des systèmes informatiques",
+                            "subtitle":"Université d'Atlantida Argentine"
+                        },
+                        {
+                            "title":"Electricien-Technicien",
+                            "subtitle":"Formation professionnelle"
+                        },
+                        {
+                            "title":"ESMET N°1",
+                            "subtitle":"Ecole technique"
+                        },
+                        {
+                            "title":"Mécanicien",
+                            "subtitle":"Formation professionnelle"
+                        }
+                    ]
+                },
+                "sect3":{
+                    "title":"Compétence"
+                },
+                "sect4":{
+                    "title":"Autre compétence",
+                    "opt":[
+                        "Résoudre des problèmes",
+                        "Analyse mathématique",
+                        "Analyse des données",
+                        "Excel Avancé",
+                        "Power Point Avancé"
+                    ]
+                },
+                "sect5":{
+                    "title":"Expérience de travail",
+                    "subtitle":"Service pénitentiaire Bs As, Argentine",
+                    "jobs":[
+                        {
+                            "title":"Technicien d'assistance",
+                            "rank":"DECEMBRE 2018 - MAINTENANT",
+                            "detail":"Développement de bases de données en vb.net & SQL. Réparation de PC, Maintenance logicielle et matérielle. Installation, modification et maintenance du réseau. Aide à la récupération d'informations et à la réinitialisation des mots de passe. "
+                        },
+                        {
+                            "title":"Entrée de données",
+                            "rank":"SEPTEMBRE 2015 – DECEMBRE 2018",
+                            "detail":"Télécharger, modifier et supprimer des données. Manipulation des données et recherche d'erreurs. "
+                        },
+                        {
+                            "title":"Technicien en électricité",
+                            "rank":"MARS 2011 – SEPTEMBRE 2015",
+                            "detail":"Maintenance de l'installation électrique et prévention des pannes futures. Montage de nouvelles installations pour les réseaux généraux et domestiques. Maintenance et démarrage du groupe électrogène. Installation des cartes principales et secondaires. "
+                        },
+                        {
+                            "title":"Sécurité",
+                            "rank":"AVRIL 2009 – MARS 2011",
+                            "detail":"Périmètre de surveillance. "
+                        }
+                    ]
+                }
+            }
+        }
+    ]
+}
--- js/script.js
+++ js/script.js
@@ -1,7 +1,98 @@
+//Detecta el idioma del sistema
+var ln = navigator.language || navigator.userLanguage;
+ln=ln.substring(0,2);
+//Selecciona el objeto del idioma
+function changeLanguage(ln){
+    let selectLang="";
+    data.languages.forEach(language => {
+        if (language.lang===ln){
+            selectLang=language;
+        };
+    });
+    return selectLang;
+};
+//Escribe la burbuja dinamica
 const typed = new Typed('.typed',{
-    strings:['echo "Hola Mundo!"','print "Hola Mundo!"','"Hola Mundo!"'],
+    strings:changeLanguage(ln).header.bubble,
     typeSpeed:75,
     cursorChar:'',
     loop:true,
     loopCount:false,
+});
+//construccion del contenido segun el lenguage
+function construction(object){
+    //se escribe el menu
+    let menu= document.querySelectorAll(".m");
+    for(let i=0;i<menu.length;i++){
+        switch (i) {
+            case 0:
+                menu[i].textContent=object.menu.title;
+                break;
+            case 1:
+                menu[i].textContent=object.menu.opt1;
+            break;
+            case 2:
+                menu[i].textContent=object.menu.opt2;
+                break;
+            case 3:
+                menu[i].textContent=object.menu.opt3;
+                break;
+            case 4:
+                menu[i].textContent=object.menu.opt4;
+                break;
+            default:
+                menu[i].textContent=object.message;
+                break;
+        }   
+    };
+    //Se escribe el titulo
+    let title=document.querySelector("#i");
+    title.textContent=object.main.sect1.title;
+    //Se escribe section 1
+    let sect1=document.querySelector("#sect1");
+    sect1.children[0].textContent=object.main.sect1.subtitle;
+    sect1.children[1].textContent=object.main.sect1.message;
+    //Se escribe section 2
+    let sect2=document.querySelector("#sect2");
+    sect2.children[0].textContent=object.main.sect2.title;
+    for (let i = 0; i < sect2.children[1].children.length; i++) {
+        sect2.children[1].children[i].children[0].textContent=object.main.sect2.load[i].title;
+        sect2.children[1].children[i].children[1].textContent=object.main.sect2.load[i].subtitle;
+    }
+    //Se escribe section 3
+    let sect3=document.querySelector("#sect3");
+    sect3.children[0].textContent=object.main.sect3.title;
+    //Se escribe section 4
+    let sect4=document.querySelector("#sect4");
+    sect4.children[0].textContent=object.main.sect4.title;
+    for (let i = 0; i < sect4.children[1].children.length; i++) {
+        sect4.children[1].children[i].children[1].textContent=object.main.sect4.opt[i];
+    }
+    //Se escribe section 5
+    let sect5=document.querySelector("#sect5");
+    sect5.children[0].textContent=object.main.sect5.title;
+    sect5.children[1].children[0].children[0].textContent=object.main.sect5.subtitle;
+    for (let i = 0; i < sect5.children[1].children[0].children[1].children.length; i++) {
+        sect5.children[1].children[0].children[1].children[i].children[0].textContent=object.main.sect5.jobs[i].title;
+        sect5.children[1].children[0].children[1].children[i].children[1].textContent=object.main.sect5.jobs[i].rank;
+        sect5.children[1].children[0].children[1].children[i].children[2].textContent=object.main.sect5.jobs[i].detail;
+    }
+};
+construction(changeLanguage(ln));
+//captura formulario
+let formLang=document.querySelector(".languages");
+//trae un array con los valores del radio
+let arrayLang=document.querySelectorAll(".language");
+arrayLang.forEach(element=>{
+    if (element.value===ln) {
+        element.checked=true;
+    }
+});
+//escucha el formulario
+formLang.addEventListener("change",(event)=>{
+    arrayLang.forEach(element=>{
+        if (element.checked){
+            construction(changeLanguage(element.value));
+        };
+    });
 });
